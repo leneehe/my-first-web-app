38f9560c3a64aa3660e5c9e6427aec84fba1794b
Lena <fatmeow@gmail.com>
2017-11-17 02:09:37 -0500

    Complete exercise with displaying instance variable and redirecting

--- server.rb
+++ server.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
 
 get '/portfolio' do
@@ -9,5 +17,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['http://wwww.netflix.com', 'http://wwww.behance.net', 'http://www.youtube.com', 'http://lookbook.nu/', 'https://www.luisaviaroma.com']
+  erb :links
 end
